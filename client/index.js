8fa279a94f1d8c844258425806d7a9df2f24a75f
Chris <chris.haack@me.com>
2017-02-23 17:05:19 -0500

    add youtube search for easy breezy api calls

--- client/index.js
+++ client/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import SearchBar from './components/SearchBar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = process.env.YOUTUBE_KEY;
+
+YTSearch({key: API_KEY, term: 'bunnies'}, function(data) {
+  console.log(data);
+})
 
 const App = () => {
   return (
